41b999ab236a48a766d6edb9a6c20233a5d1f50b
yuenkokeith <yuenkokeith@yahoo.com.hk>
2022-04-26 13:13:06 +0800

    Update SpAPI.php

--- src/SpAPI.php
+++ src/SpAPI.php
@@ -2,16 +2,6 @@
 
 	namespace yuenkokeith\spapi;
 	
-	class ApiLoader 
-	{	
-		public static function getAPI($sp_name)
-		{
-			require_once($sp_name. 'API\\' . $sp_name . 'API.php');
-			$class = 'yuenkokeith\spapi\\' . $sp_name;
-			return new $class;
-		}
-	}
-
 	class SpAPI 
 	{
 		private static $result;
